a0e9555234068ee89e9d591f6de77d4067a6d3c9
PGMacDesign <pgmacdesign@gmail.com>
2014-07-19 00:49:38 -0700

    Facebook style feed that uses volley

---
+++ gen/com/pgmacdesign/androidhive_facebookstylefeedwithvolley/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pgmacdesign.androidhive_facebookstylefeedwithvolley;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
